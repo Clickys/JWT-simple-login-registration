bbe51b4d3155423666049c3b2acae401fdcfcb56
clickys <koc.clicky@gmail.com>
2024-05-27 22:26:39 +0300

    added rateLimit,timeout and helmet to secure the app for various attacks

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const authenticateJWT = require('../middleware/authenticateJwt.js');
-
+// security middlewares
+const rateLimit = require('express-rate-limit');
+const timeout = require('connect-timeout');
+const helmet = require('helmet');
 const {
     validateRegister,
     registerController,
     loginController,
 } = require('../controllers/authController.js');
+const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+router.use(helmet());
 router.post('/register', validateRegister, registerController);
 router.post('/login', loginController);
 
-router.get('/protected', authenticateJWT, (req, res) => {
+router.get('/protected', timeout('5s'), authenticateJWT, (req, res) => {
     res.send('Protected route');
 });
 
